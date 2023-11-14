182c3ff3e62a0dd6f1b1b7b69e264ed6825ce36c
Abood Jdou <ranaahdidi96@gmail.com>
2023-11-14 19:29:51 +0300

    Adding the routing Mythology to User area in home controller

--- AreaBox-V0.1/Areas/User/Controllers/HomeController.cs
+++ AreaBox-V0.1/Areas/User/Controllers/HomeController.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
 namespace AreaBox_V0._1.Areas.User.Controllers;
+[Area("User")]
+[Route("[controller]/[action]")]
+
 public class HomeController : Controller
 {
-    [Area("User")]
+
     public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Info()
     {
         return View();
     }
