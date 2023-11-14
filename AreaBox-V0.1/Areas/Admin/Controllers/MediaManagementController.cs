4c2dbf9a5cefa012c2b7f8bacbde69754f28a1ce
motazallala <www.hot122@gmail.com>
2023-11-15 02:55:23 +0300

    adding the admin controllers to admin area

--- AreaBox-V0.1/Areas/Admin/Controllers/MediaManagementController.cs
+++ AreaBox-V0.1/Areas/Admin/Controllers/MediaManagementController.cs
@@ -3,15 +3,10 @@
 namespace AreaBox_V0._1.Areas.Admin.Controllers;
 [Area("Admin")]
 [Route("[controller]/[action]")]
-public class DashboardController : Controller
+public class MediaManagementController : Controller
 {
     public IActionResult Index()
     {
         return View();
     }
-    public IActionResult Index2()
-    {
-        return View();
-    }
 }
-
---
+++ AreaBox-V0.1/Areas/Admin/Controllers/QAManagementController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AreaBox_V0._1.Areas.Admin.Controllers;
+public class QAManagementController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
