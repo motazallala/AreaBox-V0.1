01cfa0cfc08032674df71f56cb0b416fb7f73989
motazallala <www.hot122@gmail.com>
2023-11-15 01:46:29 +0300

    add dashbord to user area

---
+++ AreaBox-V0.1/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AreaBox_V0._1.Areas.Admin.Controllers;
+[Area("Admin")]
+[Route("[controller]/[action]")]
+public class DashboardController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+
