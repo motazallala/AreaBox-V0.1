46eafdf5bbc812dac0b0c44043ff4402a1a03091
Aizen <31359882+alaaalbayed@users.noreply.github.com>
2023-11-19 08:05:40 +0300

    Add a Settings page and fix the nav

---
+++ AreaBox-V0.1/Areas/Admin/Pages/Settings/_Test.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AreaBox_V0._1.Areas.Admin.Pages.Settings
+{
+    public class _TestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
