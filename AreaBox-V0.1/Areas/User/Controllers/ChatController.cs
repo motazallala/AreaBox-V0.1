ae7bdaf00be952673169a96ef2430f685cb0177c
motazallala <www.hot122@gmail.com>
2023-12-31 13:26:29 +0300

    add new controller to user area edit yhe user media post ui

---
+++ AreaBox-V0.1/Areas/User/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AreaBox_V0._1.Areas.User.Controllers;
+[Area("User")]
+[Route("[controller]/[action]")]
+public class ChatController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
