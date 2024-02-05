018c343cb612de504f6488d62d13fb281283b61c
Abood Jdou <ranaahdidi96@gmail.com>
2024-02-06 00:57:54 +0300

    edit login logo and register add dashboard for admin controller

---
+++ AreaBox-V0.1/Areas/Admin/Models/Dashboard/send/DashboardAnalysis.cs
@@ -0,0 +1,20 @@
+﻿namespace AreaBox_V0._1.Areas.Admin.Models.Dashboard.send
+{
+    public class DashboardAnalysis
+    {
+        public int MediaPostCount { get; set; }
+        public double MediaPostsPercentage { get; set; }
+
+        public int QuestionPostCount { get; set; }
+        public double QuestionPostsPercentage { get; set; }
+        public double TotalPostsPercentage { get; set; }
+        public int UserCount { get; set; }
+
+        public int LastMonthMediaPostCount { get; set; }
+        public int Last3MonthsMediaPostCount { get; set; }
+        public int LastMonthQuestionPostCount { get; set; }
+        public int Last3MonthsQuestionPostCount { get; set; }
+
+
+    }
+}
